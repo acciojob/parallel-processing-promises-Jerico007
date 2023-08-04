67e3e7da921f37cb60a2953de59eded4dfd63f26
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-08-04 15:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+function downloadImages() {
+
+ let promises = images.map((val)=>{
+			
+		   let prom = new Promise((resolve,reject)=>{
+			   const img = new Image();
+			   img.src = val.src;
+			   img.alt = val.alt;
+ 			img.onload = ()=>{
+				resolve(val);
+			}
+
+			img.onerror = ()=>{
+				reject(`Failed to load image's URL: ${val.url}`)
+			}
+		});
+	 return prom;
+	});
+
+	 Promise.all(promises)
+	.then((data) =>{
+		data.forEach((image) =>{
+			output.appendChild(image);
+		})
+	})
+	.catch((data)=>
+		{
+			console.log(data);
+		});
+
+}
+
+
+btn.addEventListener("click" , downloadImages);
+
+
